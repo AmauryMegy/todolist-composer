3e6ff38dee112bca55b18a713ac126ac1af7b6c7
AmauryMegy <megy.amaury@gmail.com>
2022-07-13 09:45:13 +0200

    first commit

---
+++ toDoListComposer/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO List</title>
+</head>
+<body>
+    <ul>
+        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed optio harum molestias iure modi, minus sunt eius alias et. Nihil consectetur odit qui at delectus dolores inventore cupiditate obcaecati enim?</li>
+        <li>Ad ducimus deleniti reprehenderit explicabo ullam veniam ea illo. Quaerat debitis sint magnam, rem recusandae sunt eveniet mollitia suscipit minus aut magni eos optio molestiae dolores. Nemo doloremque esse expedita.</li>
+        <li>Accusantium sint nostrum est laborum reprehenderit architecto aliquam doloremque, praesentium, iusto obcaecati eos eum iste ipsum magni blanditiis ducimus autem officiis atque in cum nisi ratione. Mollitia, laborum! Doloremque, voluptatibus.</li>
+        <li>Omnis, ullam quaerat temporibus inventore quia dolore consequuntur eos recusandae in doloremque necessitatibus et porro nihil blanditiis culpa exercitationem? Dolor eum blanditiis quasi amet deleniti tempora reiciendis expedita distinctio architecto?</li>
+        <li>Sit, accusantium. Enim neque molestiae quidem earum delectus tenetur a, eaque officia, vel explicabo ipsa. Maxime veritatis laborum, quaerat excepturi expedita, id saepe ex eum, eos molestiae ipsam quibusdam amet?</li>
+    </ul>
+</body>
+</html>
